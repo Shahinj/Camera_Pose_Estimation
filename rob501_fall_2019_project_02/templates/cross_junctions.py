done the jacobian, need to fix junctions

rob501_fall_2019_project_02/templates/cross_junctions.py
@@ -121,19 +121,12 @@ def cross_junctions(I, bounds, Wpts):
 
 
 
-# def plot(I):
-#     import matplotlib.pyplot as plt
-#     plt.imshow(I, cmap = 'gray')
-#     plt.show()
+def plot(I):
+    import matplotlib.pyplot as plt
+    plt.imshow(I, cmap = 'gray')
+    plt.show()
     
-#     
-# def null_space_calc(U):
-#     # find the eigenvalues and eigenvector of U(transpose).U
-#     e_vals, e_vecs = np.linalg.eig(np.dot(U.T, U))  
-#     # extract the eigenvector (column) associated with the minimum eigenvalue
-    return e_vecs[:, np.argmin(e_vals)] 
-    
-    
+
 def find_neighbours(I, pt):
     to_check_q = []
     visited = []
@@ -192,32 +185,20 @@ def remove_closest_to_bounds(points, bounds):
     distances = np.zeros( shape = (len(points),6))
     result = np.zeros( shape = (0,2))
     for i,point in enumerate(points):
-        if point[0,0] == 240:
-            print('hi')
-        top_d   = distance_to_line(point, bounds[:,1],bounds[:,0])[0]
+        top_d   = distance_to_line(point, bounds[:,1],bounds[:,0])
 
-        right_d = distance_to_line(point, bounds[:,2],bounds[:,1])[0]
+        right_d = distance_to_line(point, bounds[:,2],bounds[:,1])
         
-        bot_d   = distance_to_line(point, bounds[:,3],bounds[:,2])[0]
+        bot_d   = distance_to_line(point, bounds[:,3],bounds[:,2])
         
-        left_d  = distance_to_line(point, bounds[:,0],bounds[:,3])[0]
+        left_d  = distance_to_line(point, bounds[:,0],bounds[:,3])
 
-   #       
-        distances[i,:]= [point[0,0], point[1,0] ,top_d,right_d, bot_d, left_d] 
+        distances[i,:]= [point[0], point[1] ,top_d,right_d, bot_d, left_d] 
 
-   #   
     
     distances = np.hstack([distances,distances[:,2:].min(axis = 1).reshape(len(distances),1)])
-    # distances = np.hstack([distances,distances[:,2:-1].prod(axis = 1).reshape(len(distances),1)])
-    
     return distances[distances[:,-1].argsort()][-48:,:2]
-    # distances = np.zeros( shape = (len(points),3))
-    # centroid = np.array([[np.mean(bpoly[0,:])] , [np.mean(bpoly[1,:])]])
-    # for i,point in enumerate(points):
-    #     if point[0,0] == 240:
-    #         print('hi')
-    #     distances[i,:]= [point[0,0], point[1,0] , np.sqrt(np.sum((point - centroid) ** 2)) ] 
-    # return distances[distances[:,-1].argsort()][:48,:2]
+
     
 def saddle_point(I):
     """
@@ -272,23 +253,141 @@ def saddle_point(I):
     return pt
     
     
+def bilinear_interp(I, pt):
+    """
+    Performs bilinear interpolation for a given image point.
+
+    Given the (x, y) location of a point in an input image, use the surrounding
+    4 pixels to conmpute the bilinearly-interpolated output pixel intensity.
+
+    Note that images are (usually) integer-valued functions (in 2D), therefore
+    the intensity value you return must be an integer (use round()).
+
+    This function is for a *single* image band only - for RGB images, you will 
+    need to call the function once for each colour channel.
+
+    Parameters:
+    -----------
+    I   - Single-band (greyscale) intensity image, 8-bit np.array (i.e., uint8).
+    pt  - 2x1 np.array of point in input image (x, y), with subpixel precision.
+
+    Returns:
+    --------
+    b  - Interpolated brightness or intensity value (whole number >= 0).
+    """
+    #--- FILL ME IN ---
+
+    if pt.shape != (2, 1):
+        raise ValueError('Point size is incorrect.')
+    
+    #Get the x and y value from the point
+    x = pt[0,0]
+    y = pt[1,0]
+    #define the x_vector
+    x_vec = np.array([[1],[x],[y],[x*y]])
+    #define the lower and upper bounds of x and y
+    x_low = np.ceil(x).astype(int)  - 1
+    x_up = np.floor(x).astype(int)  + 1
+    y_low = np.ceil(y).astype(int)  - 1
+    y_up = np.floor(y).astype(int)  + 1
     
 
+    #x is across columns of the image, y is across rows, also clip to get the nearest pixel (np)
+    x_low_np = np.clip( x_low ,0, I.shape[1] - 1)
+    x_up_np  = np.clip( x_up  ,0, I.shape[1] - 1) 
+    y_low_np = np.clip( y_low ,0, I.shape[0] - 1) 
+    y_up_np  = np.clip( y_up  ,0, I.shape[0] - 1) 
+    
+    #get the values for the 4 nearest points from the Image
+    left_down  = I[y_low_np,x_low_np]
+    right_down = I[y_low_np,x_up_np ]
+    left_up    = I[y_up_np ,x_low_np]
+    right_up   = I[y_up_np ,x_up_np ]
+    
+    #get the B coefficients
+    b_coefs = np.linalg.inv(np.array([
+                        [1,x_low, y_low, x_low * y_low],
+                        [1,x_low, y_up , x_low * y_up ],
+                        [1,x_up , y_low, x_up  * y_low],
+                        [1,x_up , y_up , x_up  * y_up ]
+                    ])).T.dot(x_vec)
+    #calculate the value of b and round, make sure it is >= 0
+    b =  np.max(np.round(b_coefs.T.dot(np.array([[left_down], [left_up], [right_down], [right_up]]))), 0)[0]
+    #------------------
 
-# def move_centroids(points, closest, centroids):
-#     """returns the new centroids assigned from the points closest to them"""
-#     return np.array([points[closest==k].mean(axis=0) for k in range(centroids.shape[0])])
-#     
-# def closest_centroid(points, centroids):
-#     """returns an array containing the index to the nearest centroid for each point"""
-#     distances = np.sqrt(((points - centroids[:, np.newaxis])**2).sum(axis=2))
-#     return np.argmin(distances, axis=0)
-#     
-# def initialize_centroids(points, k):
-#     """returns k centroids from the initial points"""
-#     centroids = points.copy()
-#     np.random.shuffle(centroids)
-#     return centroids[:k]
+    return b
+
+
+def dlt_homography(I1pts, I2pts):
+    """
+    Find perspective Homography between two images.
+
+    Given 4 points from 2 separate images, compute the perspective homography
+    (warp) between these points using the DLT algorithm.
+
+    Parameters:
+    ----------- 
+    I1pts  - 2x4 np.array of points from Image 1 (each column is x, y).
+    I2pts  - 2x4 np.array of points from Image 2 (in 1-to-1 correspondence).
+
+    Returns:
+    --------
+    H  - 3x3 np.array of perspective homography (matrix map) between image coordinates.
+    A  - 8x9 np.array of DLT matrix used to determine homography.
+    """
+    #--- FILL ME IN ---
+    #initialize the A matrix with zeros
+    A = np.zeros([8,9])
+    #loop through every point in source and create the A matrix, 2 row by 2 row
+    for i,point in enumerate(I1pts.T):
+        x = point[0]
+        y = point[1]
+        #get the corresponce point from the input destinations points
+        correspondence = I2pts.T[i]
+        u = correspondence[0]
+        v = correspondence[1]
+        #construct the A matrix use the formula in the paper
+        A_i = np.array([ [-1*x, -1*y, -1,   0 ,   0 ,  0, u*x, u*y, u],
+                         [0   ,    0,  0, -1*x, -1*y, -1, v*x, v*y, v],
+                        ])
+        #add these 2 rows to the resultant A matrix (the one we initialize at the beginning)
+        A[i*2 : (i+1)*2,:] = A_i
+    
+    #calculate the H matrix using null space
+    H = null_space_calc(A)
+    #reshape the H matrix
+    H = H.reshape(3,3)
+    #get the last entry and normalize
+    last_entry = H[2][2]
+    #normalize
+    H = H * (1.0/last_entry)
+
+    
+    #------------------
+
+    return H, A
+    
+def null_space_calc(U):
+    # find the eigenvalues and eigenvector of U(transpose).U
+    e_vals, e_vecs = np.linalg.eig(np.dot(U.T, U))  
+    # extract the eigenvector (column) associated with the minimum eigenvalue
+    return e_vecs[:, np.argmin(e_vals)] 
+
+
+def move_centroids(points, closest, centroids):
+    """returns the new centroids assigned from the points closest to them"""
+    return np.array([points[closest==k].mean(axis=0) for k in range(centroids.shape[0])])
+    
+def closest_centroid(points, centroids):
+    """returns an array containing the index to the nearest centroid for each point"""
+    distances = np.sqrt(((points - centroids[:, np.newaxis])**2).sum(axis=2))
+    return np.argmin(distances, axis=0)
+    
+def initialize_centroids(points, k):
+    """returns k centroids from the initial points"""
+    centroids = points.copy()
+    np.random.shuffle(centroids)
+    return centroids[:k]
 #     
 # def is_in_polygon(pt,pbounds, I_shape):
 #     line = np.mgrid[pt[0,0]: I_shape[1],pt[1,0]:pt[1,0] + 1]
@@ -336,96 +435,195 @@ def point_in_poly(pt,poly):
    else:
       return False
    
+def apply_homography(I,bounds,to_convert):
+    Ihack = np.zeros((255,255))
+    H,A = dlt_homography(to_convert,bounds)
+
+    #loop through the bounding box
+    for x in     range(np.min(to_convert.T[:,0]) ,np.max(to_convert.T[:,0]) ):
+        for y in range(np.min(to_convert.T[:,1]) ,np.max(to_convert.T[:,1]) ):
+            #define the homogenous point of the billboard
+            homogenous_point = np.array([[x,y,1.0]]).T
+            #get the corresponding point in the soldiers tower
+            correspondence = H.dot(homogenous_point)
+            #normalize the correspondence point by w
+            correspondence = correspondence / correspondence[2,0]
+            #bilinearly interpolate the soldiers tower
+            interpolated = bilinear_interp(I,correspondence[:-1,0:])
+            #set the intensity of the yd picture to st
+            Ihack[y, x] = interpolated
+            
+    return Ihack
+    
+import os
+os.chdir(r'C:\Users\Shahin\Documents\School\Skule\Year 4\Fall\ROB501\Camera_Pose_Estimation\rob501_fall_2019_project_02\templates')
+
+if __name__ == "__main__":
+
+
+
+    import numpy as np
+    from imageio import imread
+    from mat4py import loadmat
+    from cross_junctions import cross_junctions
+    
+    # Load the boundary.
+    bpoly = np.array(loadmat("bounds.mat")["bpolyh1"])
+    
+    # Load the world points.
+    Wpts = np.array(loadmat("world_pts.mat")["world_pts"])
+    
+    # Load the example target image.
+    I = imread("example_target.png")
+    
+    # Ipts = cross_junctions(I, bpoly, Wpts)
+    # mid_junctions = remove_closest_to_bounds(centroids,bpoly)
+    m, n = I.shape
+    
+    
+    start_x,start_y = 12,7
+    junctions = []
+    for down in range(0,6):
+        for right in range(0,8):
+            poi = saddle_point(Ihack[start_y + (34*down) : start_y + ((down+2)*34),start_x + (25*right) : start_x + ((right+2)*25)])
+            junctions.append( [ start_x + (34*right) + poi[0,0] , start_y + (34*down) + poi[1,0]])
+    
+    #homography part
+    # Compute the perspective homography we need...
+    to_convert = np.array([[  0, 255, 255,   0],
+           [  0,   0, 255, 255]])
+           
+           
+    Ihack = np.zeros((255,255))
+    H,A = dlt_homography(to_convert,bpoly)
+    
+    tc_list = [(x,y) for x in range(0,255) for y in range(0,255)]
+    tc_coords = np.array(tc_list).T
+    tc_coords_h = np.vstack([tc_coords,[1] * 255 * 255])
+    tc_homographied = H.dot(tc_coords_h)
+    tc_homographied_norm = tc_homographied[:2,:] / tc_homographied[2,:]
+        
+    for i in range(0,tc_homographied_norm.shape[1]):
+        x,y = tc_coords[:,i]
+        Ihack[y, x] =  bilinear_interp(I,tc_homographied_norm[:,i:i+1])
+    
+    start_x,start_y = 12,7
+    junctions = []
+    for down in range(0,6):
+        for right in range(0,8):
+            poi = saddle_point(Ihack[start_y + (34*down) : start_y + ((down+2)*34),start_x + (25*right) : start_x + ((right+2)*25)])
+            junctions.append( [ start_x + (34*right) + poi[0,0] , start_y + (34*down) + poi[1,0]])
+    
+    for item in junctions:
+        Ihack[ int(item[1]), int(item[0]) ] = 255
+    
+    
+    #loop through the bounding box
+    for x in     range(0 ,255):
+        for y in range(0 ,255):
+            #define the homogenous point of the billboard
+            homogenous_point = np.array([[x,y,1.0]]).T
+            #get the corresponding point in the soldiers tower
+            correspondence = H.dot(homogenous_point)
+            #normalize the correspondence point by w
+            correspondence = correspondence / correspondence[2,0]
+            #bilinearly interpolate the soldiers tower
+            interpolated = bilinear_interp(I,correspondence[:-1,0:])
+            #set the intensity of the yd picture to st
+            Ihack[y, x] = interpolated
+    #        
+    # I_homography,H = apply_homography(I,bpoly,to_convert)
+     
+    
+    
+    
+    
+    
+    
+    dx,dy = np.gradient(Ihack)
+    
+    Ix = gaussian_filter(dx,1)
+    Iy = gaussian_filter(dy,1)
+    
+    R_score = np.zeros(Ihack.shape)
 
     
-# import os
-# os.chdir(r'C:\Users\Shahin\Documents\School\Skule\Year 4\Fall\ROB501\Camera_Pose_Estimation\rob501_fall_2019_project_02\templates')
-# 
-# if __name__ == "__main__":
-# 
-#     import numpy as np
-#     from imageio import imread
-#     from mat4py import loadmat
-#     from cross_junctions import cross_junctions
-#     
-#     # Load the boundary.
-#     bpoly = np.array(loadmat("bounds.mat")["bpolyh1"])
-#     
-#     # Load the world points.
-#     Wpts = np.array(loadmat("world_pts.mat")["world_pts"])
-#     
-#     # Load the example target image.
-#     I = imread("example_target.png")
-#     
-#     # Ipts = cross_junctions(I, bpoly, Wpts)
-#     mid_junctions = remove_closest_to_bounds(junctions,bpoly)
-#     m, n = I.shape
-#     
-#     dx,dy = np.gradient(I)
-#     
-#     Ix = gaussian_filter(dx,2)
-#     Iy = gaussian_filter(dy,2)
-#     
-#     R_score = np.zeros(I.shape)
-# 
-#     
-#     alpha = -1 * 0.0000005
-#     
-#     A = np.zeros(shape = (m,n,2,2))
-#     A[:,:,0,0] = gaussian_filter(Ix ** 2,2)
-#     A[:,:,0,1] = gaussian_filter(Ix * Iy,2)
-#     A[:,:,1,0] = gaussian_filter(Ix * Iy,2)
-#     A[:,:,1,1] = gaussian_filter(Iy ** 2,2)
-#         
-#     evals, evecs = np.linalg.eig(A)
-#     
-#     R_score[:,:] = (evals[:,:,0] * evals[:,:,1]) - alpha * ((evals[:,:,0] + evals[:,:,1])** 2) 
-#     R_thresh = (R_score > np.percentile(R_score,99)) * R_score
-#     
-#     # R_thresh = maximum_filter(R_thresh,5)
-# 
-#     junctions = []
-#     grouped = []
-#     poligono = [tuple(i) for i in bpoly.T.tolist()]
-#     for y in range(0,m):
-#         for x in range(0,n):
-#             if((x,y) in grouped):
-#                 continue
-#             pt = np.array([[x],[y]])
-#             if(R_thresh[y][x] > 0 and point_in_poly(pt, poligono)):
-#                 neighbours = find_neighbours(R_thresh, np.array([[x],[y]]))
-#                 grouped= grouped + neighbours
-#                 avg = np.array(neighbours).mean(axis = 0)
-#                 junctions.append(np.array([[avg[0]],[avg[1]]]))
-#                 # junctions.append(np.array([[x],[y]]))
-#                 
-#     # junctions = np.array(junctions)
-#     # junctions = junctions.T.reshape(junctions.shape[1],junctions.shape[0])
-#     # for i in range(0,500):
-#     #     centroids = initialize_centroids(junctions.T,48)
-#     #     closest = closest_centroid(junctions.T, centroids)
-#     #     centroids = move_centroids(junctions.T, closest, centroids)
-# 
-#     #     
-#     # 
-#     # A = np.array([[Ix ** 2, Ix * Iy],[Ix * Iy, Iy ** 2]])
-#     
-#     detected = I.copy()
-#     mid_junctions = remove_closest_to_bounds(junctions,bpoly)
-#     
-#     for i,row in enumerate(mid_junctions):
-#         x,y = mid_junctions[i]
-#         x,y = int(x), int(y)
-#         window = 20
-#         while(True):
-#             # poi = np.array([[1],[1]])
-#             poi = saddle_point(I[y-window:y+window,x-window:x+window])
-#             sx,sy = poi.T[0]
-#             if(sx > 0 and sy > 0):
-#                 break
-#             else:
-#                 window += 5
-#         detected[y - window + int(poi[1,0]),x - window + int(poi[0,0])] = 255
-#     plot(detected)    
-#     # You can plot the points to check!
-#     # print(Ipts)                
+    #alpha = -1 * 0.000000000001
+    alpha = 0.03
+    
+    A = np.zeros(shape = (Ihack.shape[0],Ihack.shape[1],2,2))
+    A[:,:,0,0] = gaussian_filter(Ix ** 2,2)
+    A[:,:,0,1] = gaussian_filter(Ix * Iy,2)
+    A[:,:,1,0] = gaussian_filter(Ix * Iy,2)
+    A[:,:,1,1] = gaussian_filter(Iy ** 2,2)
+        
+    evals, evecs = np.linalg.eig(A)
+    
+    R_score[:,:] = (evals[:,:,0] * evals[:,:,1]) - alpha * ((evals[:,:,0] + evals[:,:,1])** 2) 
+    #(np.linalg.det(A) - alpha * (np.trace(A.T).T) ** 2)
+    #(evals[:,:,0] * evals[:,:,1]) - alpha * ((evals[:,:,0] + evals[:,:,1])** 2) 
+    R_thresh = (R_score > np.percentile(R_score,90)) * R_score
+    
+    R_thresh = maximum_filter(R_thresh,10)
+
+    # junctions = []
+    # grouped = []
+    # poligono = [tuple(i) for i in bpoly.T.tolist()]
+    # for y in range(0,m):
+    #     for x in range(0,n):
+    #         if((x,y) in grouped):
+    #             continue
+    #         pt = np.array([[x],[y]])
+    #         if(R_thresh[y][x] > 0 and point_in_poly(pt, poligono)):
+    #             neighbours = find_neighbours(R_thresh, np.array([[x],[y]]))
+    #             grouped= grouped + neighbours
+    #             avg = plot(R.array(neighbours).mean(axis = 0)
+    #             junctions.append(np.array([[avg[0]],[avg[1]]]))
+                
+                
+    junctions = []
+    # poligono = [tuple(i) for i in bpoly.T.tolist()]
+    for y in range(0,Ihack.shape[0]):
+        for x in range(0,Ihack.shape[1]):
+            # pt = np.array([[x],[y]])
+            if(R_thresh[y][x] > 0): 
+            # and point_in_poly(pt, poligono)):
+                junctions.append(np.array([[x],[y]]))
+                
+    junctions = np.array(junctions)
+    junctions = junctions.T.reshape(junctions.shape[1],junctions.shape[0])
+    centroids = initialize_centroids(junctions.T,48)
+    eps = 1
+    while(True):
+        closest = closest_centroid(junctions.T, centroids)
+        new_centroids = move_centroids(junctions.T, closest, centroids)
+        if(np.sqrt(np.sum((new_centroids - centroids) ** 2)) < eps):
+            break
+        else:
+            centroids = new_centroids
+
+    #     
+    # 
+    # A = np.array([[Ix ** 2, Ix * Iy],[Ix * Iy, Iy ** 2]])
+    
+    detected = Ihack.copy()
+    mid_junctions = remove_closest_to_bounds(centroids,bpoly)
+    
+    for i,row in enumerate(mid_junctions):
+        x,y = mid_junctions[i]
+        x,y = int(x), int(y)
+        # window = 20
+        window = 0
+        # while(True):
+            # poi = np.array([[1],[1]])
+        poi = saddle_point(I[y-window:y+window,x-window:x+window])
+            # sx,sy = poi.T[0]
+            # if(sx > 0 and sy > 0):
+                # break
+            # else:
+                # window += 5
+        detected[y - window + int(poi[1,0]),x - window + int(poi[0,0])] = 255
+    plot(detected)    
+    # You can plot the points to check!
+    # print(Ipts)        
+          

rob501_fall_2019_project_02/templates/find_jacobian.py
@@ -19,46 +19,138 @@ def find_jacobian(K, Twc, Wpt):
     J  - 2x6 np.array, Jacobian matrix (columns are tx, ty, tz, r, p, q).
     """
     #--- FILL ME IN ---
-    # k_inv = np.linalg.inv(K)
-    # k_inv_stacked = np.vstack([k_inv, [0,0,0]])
-    # C = Twc[0:3,0:3]
-    # t = Twc[0:3,3]
-    # 
-    # Tcw = np.hstack([C.T, (-1 * C.T.dot(t)).reshape(3,1) ])
-    # Tcw = np.vstack([Tcw, [0,0,0,1]])
-    # 
-    # wpt_h = np.vstack([Wpt,[1]])
-    # 
-    # xij_h = K.dot(Tcw.dot(wpt_h)[0:3,:])
-    # xij_h_norm = xij_h / xij_h[2,0]
-    # xij = xij_h_norm[0:2]
-    # 
-    # 
-    # wpt_calc = C.T.dot(k_inv.dot(xij_h))
-    # wpt_calc = np.vstack([wpt_calc,[1]])
-    # #translation
-    # wpt_calc = np.vstack([np.hstack([np.eye(3),Tcw[0:3,3].reshape(3,1)]),[0,0,0,1]]).dot(wpt_calc)
-    # 
-    # #using Szeliski, Page 286 notation
-    # y_1 = Wpt - Twc[0:3,3].reshape((3,1))
-    # y_2 = Twc[0:3,0:3].dot(y_1)
-    # y_3 = y_2 / y_2[2,0]
-    # xi = K.dot(y_3)
-    # 
-    # y_2_z = y_2[2,0]
-    # dy2_d1 = (y_2 / y_2_z - np.array([[0],[0],[1]]))
-    # dy1_dx = np.array([ [0, 0, 0, K[0,0]], [0, 0, 0, 0], [0, 0, 0, 0]])
-    # #])
-    # 
-    # wpt_h = np.vstack([Wpt,[1]])
-    # 
-    # 
-    # y_1 = Wpt - Twc[0:3,3].reshape((3,1))
-    # y_2 = np.gradient(Twc[0:3,0:3])[0].dot(y_1) 
-    # y_3 = y_2 / y_2[2,0]
-    # xi = K.dot(y_3)
-    # 
-    # J = np.zeros((2,6))
+    R = Twc[0:3,0:3]
+    T = Twc[0:3,3]
+    tx,ty,tz = T
+    wx,wy,wz = Wpt.reshape(3)
+    r1,r2,r3,r4,r5,r6,r7,r8,r9 = R.flatten()
+    fx,s,cx = K[0,:].flatten()
+    fy,cy = K[1,1:].flatten()
+    
+    #a represents -1*R^(-1)*T
+    a1 = -1 * (r1 * tx + r4 * ty + r7 * tz)
+    a2 = -1 * (r2 * tx + r5 * ty + r8 * tz)
+    a3 = -1 * (r3 * tx + r6 * ty + r9 * tz)
+        
+        
+    q = np.arctan2(r4,r1)                           #psi
+    p = np.arctan2(-1*r7,np.sqrt(1-r7 ** 2))        #theta
+    r = np.arctan2(r8,r9)                           #phi
+    
+    denom = r3 * wx + r6 * wy + r9 * wz + a3
+    ix_num = wx*(fx*r1 + s*r2 + cx*r3) + wy*(fx*r4 + s*r5 + cx*r6) + wz*(fx*r7 + s*r8 + cx*r9) + fx*a1 + s*a2 + cx*a3
+    iy_num = wx*(fy*r2 + cy*r3) + wy*(fy*r5 + cy*r6) + wz*(fy*r8 + cy*r9) + fy*a2 + cy*a3
+
+
+    #tx, happens in a1,a2,a3
+    d_denom = -r3
+    #x
+    d_num = (fx * -1 * r1) + (s * -1 * r2) + (cx * -1 * r3)
+    dx_dtx = (denom * d_num - d_denom * ix_num) / (denom ** 2)
+    #y
+    d_num = (fy * -1 * r2) + (cy * -1 * r3)
+    dy_dtx = (denom * d_num - d_denom * iy_num) / (denom ** 2)
+    
+    #ty, happens in a1,a2,a3
+    d_denom = -r6
+    #x
+    d_num = (fx * -1 * r4) + (s * -1 * r5) + (cx * -1 * r6)
+    dx_dty = (denom * d_num - d_denom * ix_num) / (denom ** 2)
+    #y
+    d_num = (fy * -1 * r5) + (cy * -1 * r6)
+    dy_dty = (denom * d_num - d_denom * iy_num) / (denom ** 2)
+    
+    
+    #tz, happens in a1,a2,a3
+    d_denom = -r9
+    #x
+    d_num = (fx * -1 * r7) + (s * -1 * r8) + (cx * -1 * r9)
+    dx_dtz = (denom * d_num - d_denom * ix_num) / (denom ** 2)
+    #y
+    d_num = (fy * -1 * r8) + (cy * -1 * r9)
+    dy_dtz = (denom * d_num - d_denom * iy_num) / (denom ** 2)
+    
+    cos = np.cos
+    sin = np.sin
+    phi,theta,psi = r,p,q
+    
+    
+    cq = np.array([[cos(psi), -1*sin(psi), 0],
+                   [sin(psi), cos(psi),    0],
+                   [0,         0,          1]])
+                   
+    cp = np.array([[cos(theta)       , 0       ,        sin(theta) ],
+                   [0                , 1       ,        0          ],
+                   [-1*sin(theta)    , 0       ,       cos(theta)  ]])
+                   
+    cr = np.array([[1       , 0       , 0             ],
+                   [0       , cos(phi),    -1*sin(phi)],
+                   [0       , sin(phi),       cos(phi)]])
+    
+    
+    
+    #q (psi)
+    dcq = np.array([[-1*sin(psi), -1*cos(psi),    0],
+                    [cos(psi)   , -1*sin(psi),    0],
+                    [0          ,           0,    0]])
+                    
+    dr = dcq.dot(cp.dot(cr))
+    dr1,dr2,dr3,dr4,dr5,dr6,dr7,dr8,dr9 = dr.flatten()
+    da1 = -1*(tx*dr1 +ty*dr4 +tz*dr7) 
+    da2 = -1*(tx*dr2 +ty*dr5 +tz*dr8) 
+    da3 = -1*(tx*dr3 +ty*dr6 +tz*dr9) 
+    d_denom = dr3 * wx + dr6 * wy + dr9 * wz + da3
+    
+    #x
+    d_num = wx*(fx  * dr1 + s * dr2 + cx * dr3) + wy*(fx*dr4 + s*dr5 + cx*dr6) + wz*(fx*dr7 + s*dr8 + cx*dr9) + fx * da1 + s * da2 + cx * da3
+    dx_dtq = (denom * d_num - d_denom * ix_num) / (denom ** 2)
+    #y
+    d_num = wx*(fy * dr2 + cy * dr3) + wy*(fy*dr5 + cy*dr6) + wz*(fy*dr8 + cy*dr9) + fy * da2 + cy * da3
+    dy_dtq = (denom * d_num - d_denom * iy_num) / (denom ** 2)
+    
+    #p (theta)
+    dcp = np.array([[-1*sin(theta)       , 0       ,        cos(theta) ],
+                   [0                    , 0       ,        0          ],
+                   [-1*cos(theta)        , 0       ,    -1*sin(theta)  ]])
+                    
+    dr = cq.dot(dcp.dot(cr))
+    dr1,dr2,dr3,dr4,dr5,dr6,dr7,dr8,dr9 = dr.flatten()
+    da1 = -1*(tx*dr1 +ty*dr4 +tz*dr7) 
+    da2 = -1*(tx*dr2 +ty*dr5 +tz*dr8) 
+    da3 = -1*(tx*dr3 +ty*dr6 +tz*dr9) 
+    d_denom = dr3 * wx + dr6 * wy + dr9 * wz + da3
+    
+    #x
+    d_num = wx*(fx  * dr1 + s * dr2 + cx * dr3) + wy*(fx*dr4 + s*dr5 + cx*dr6) + wz*(fx*dr7 + s*dr8 + cx*dr9) + fx * da1 + s * da2 + cx * da3
+    dx_dtp = (denom * d_num - d_denom * ix_num) / (denom ** 2)
+    #y
+    d_num = wx*(fy * dr2 + cy * dr3) + wy*(fy*dr5 + cy*dr6) + wz*(fy*dr8 + cy*dr9) + fy * da2 + cy * da3
+    dy_dtp = (denom * d_num - d_denom * iy_num) / (denom ** 2)
+    
+    #r (phi)
+    dcr = np.array([[0       , 0        , 0             ],
+                   [0       ,-1*sin(phi),    -1*cos(phi)],
+                   [0       ,   cos(phi),    -1*sin(phi)]])
+                    
+    dr = cq.dot(cp.dot(dcr))
+    dr1,dr2,dr3,dr4,dr5,dr6,dr7,dr8,dr9 = dr.flatten()
+    da1 = -1*(tx*dr1 +ty*dr4 +tz*dr7) 
+    da2 = -1*(tx*dr2 +ty*dr5 +tz*dr8) 
+    da3 = -1*(tx*dr3 +ty*dr6 +tz*dr9) 
+    d_denom = dr3 * wx + dr6 * wy + dr9 * wz + da3
+    
+    #x
+    d_num = wx*(fx  * dr1 + s * dr2 + cx * dr3) + wy*(fx*dr4 + s*dr5 + cx*dr6) + wz*(fx*dr7 + s*dr8 + cx*dr9) + fx * da1 + s * da2 + cx * da3
+    dx_dtr = (denom * d_num - d_denom * ix_num) / (denom ** 2)
+    #y
+    d_num = wx*(fy * dr2 + cy * dr3) + wy*(fy*dr5 + cy*dr6) + wz*(fy*dr8 + cy*dr9) + fy * da2 + cy * da3
+    dy_dtr = (denom * d_num - d_denom * iy_num) / (denom ** 2)
+    
+
+    
+    
+    J = np.array([ [dx_dtx, dx_dty, dx_dtz, dx_dtr, dx_dtp, dx_dtq],
+                   [dy_dtx, dy_dty, dy_dtz, dy_dtr, dy_dtp, dy_dtq]])
 
     #------------------
 